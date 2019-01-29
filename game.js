5271cf300641251ba390bdb1f9a18f9d38cebe1f
alexisspa9 <alexisspa9@gmail.com>
2019-01-29 14:32:20 +0200

    fixed performance

--- game.js
+++ game.js
@@ -1,40 +1,40 @@
+// store the grid container
+let grid = document.getElementById("gridContainer");
+// Store the button elements
+let clearButton = document.getElementById("clear");
+let newButton = document.getElementById("new");
+clearButton.addEventListener("click", clearGrid);
+newButton.addEventListener("click", newGrid);
+let rows = [];
+let cols = [];
 /* 
  * Starts the game. 
  * @param {number} size The size of the grid for example 16x16. 
 */
-function gameStart(size) {
-	// store the grid container
-	let grid = document.getElementById("gridContainer");
-	// Store the button elements
-	let clearButton = document.getElementById("clear");
-	let newButton = document.getElementById("new");
-	clearButton.addEventListener("click", clearGrid);
-	newButton.addEventListener("click", newGrid);
-	// declare width and height size of box element
-	let widthSize = 960 / size;
-	let heightSize = 500 / size;
-
+function gameStart(size = 16) {
 	// creates the grid__box elements and appends them to the grid container
-	for(let i = 0; i < (size * size); i++) {
-		let ele = gridElement();
-		ele.style.width = `${widthSize}px`;
-		ele.style.height = `${heightSize}px`;
-		grid.appendChild(ele);
-		
-		// width: calc(960px / 4);
-  		// height: calc(960px / 4);
+	for(let i = 0; i < size; i++) {
+		rows[i] = document.createElement("div");
+		rows[i].classList.add("row");
+		rows[i].style.height = `calc(100% / ${size})`;
+		grid.appendChild(rows[i]);
 	}
-
-
-	// Run the Mouse over Listeners
-	runListeners();
-
-
-
-
-
+	for(let i = 0; i < size; i++) {
+	   for(let j = 0; j < size; j++) {
+            cols[j] = document.createElement("div");
+            cols[j].classList.add("cols");
+            rows[i].appendChild(cols[j]);
+            
+            rows[i].children[j].addEventListener('mouseover', changeColor);
+            
+        }
+	}
 }
 
+function changeColor(e) {
+	let colors = ["red", "yellow", "blue", "green", "aqua", "fuchsia"];
+	e.target.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+}
 /* 
  * Creates a grid element.  
 */
@@ -49,10 +49,11 @@ function gridElement() {
  * Clears the Grid
 */
 function clearGrid() {
-	let boxes = Array.from(document.querySelectorAll(".grid__box"));
-	for(let box of boxes) {
-		box.style.background = "white";
+	var myNode = document.getElementById("gridContainer");
+	while (myNode.firstChild) {
+	    myNode.removeChild(myNode.firstChild);
 	}
+	gameStart();
 }
 
 /* 
@@ -74,20 +75,5 @@ function newGrid() {
 	}
 }
 
-function runListeners() {
-	// store box elements in an array
-	let boxes = Array.from(document.querySelectorAll(".grid__box"));
-	for(let i = 0; i < boxes.length; i++) {
-		boxes[i].addEventListener('mouseover', changeColor);
-	}
-}
-
-function changeColor(e) {
-	let colors = ["red", "yellow", "blue", "green", "aqua", "fuchsia"];
-	e.target.style.background = colors[Math.floor(Math.random() * colors.length)];
-}
-
-
-
-gameStart(16);
+gameStart();
 
